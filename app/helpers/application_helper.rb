73695007ee4b9cf286f5123307bde92008914704
rmesser30 <rmesser30@yahoo.com>
2017-07-28 09:50:44 -0600

    intergrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,5 +13,9 @@ module ApplicationHelper
   		greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
   		content_tag(:p, greeting, class: "source-greeting")
   	end
+  end 
+
+  def copyright_generator
+    MesserViewTool::Render.copyright 'Rick Messer','Make It Happen'
   end
 end
